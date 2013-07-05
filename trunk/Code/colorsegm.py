cec70f0d6878edd9856116aac779a9074a97471b
aya.skamel@gmail.com <aya.skamel@gmail.com@03c02e71-6919-b155-f245-4134f577a48b>
2013-07-05 17:32:51 +0000

    Detection Improvement

--- trunk/Code/colorsegm.py
+++ trunk/Code/colorsegm.py
@@ -3,7 +3,11 @@ import cv2
 def segmentRed(cv_im):
 	hsv_im = cv2.cvtColor(cv_im, cv2.COLOR_BGR2HSV)
 	h, s, v = cv2.split(hsv_im)
-	r, h = cv2.threshold(h, 30, 255, cv2.THRESH_TOZERO)
-	r, h = cv2.threshold(h, 200, 255, cv2.THRESH_TOZERO_INV)
-	r, h = cv2.threshold(h, 128, 255, 0)
-	return h
+	r, sat_mask = cv2.threshold(s, 75, 1, cv2.THRESH_BINARY)
+	# cv2.imwrite("../Temp/sat_mask.jpg", sat_mask)
+	r, h1 = cv2.threshold(h, 10, 255, cv2.THRESH_BINARY_INV)
+	r, h2 = cv2.threshold(h, 230, 255, cv2.THRESH_BINARY)
+	h = h1+h2
+	h = h*sat_mask
+	# r, h = cv2.threshold(h, 128, 255, 0)
+	return h, sat_mask
