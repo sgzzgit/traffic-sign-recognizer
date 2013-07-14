cd624ec7856900df99a64626bf4c2913640268c5
aya.skamel@gmail.com <aya.skamel@gmail.com@03c02e71-6919-b155-f245-4134f577a48b>
2013-07-14 14:41:11 +0000

    Generate more videos and applied detection to them

--- trunk/Code/tsr.py
+++ trunk/Code/tsr.py
@@ -4,20 +4,22 @@ from hough import *
 from dstCorrelation import *
 import os
 
-vid = cv2.VideoCapture("../Videos/bigtest.mp4")
 # vid = cv2.VideoCapture("../Videos/street.mp4")
-# vid = cv2.VideoCapture("../Videos/street2.avi")
+vid = cv2.VideoCapture("../Videos/street5.avi")
 
 nFrames = int(vid.get(cv.CV_CAP_PROP_FRAME_COUNT))
 fwidth = int(vid.get(cv.CV_CAP_PROP_FRAME_WIDTH))
 fheight = int(vid.get(cv.CV_CAP_PROP_FRAME_HEIGHT))
-# writer = cv2.VideoWriter("../Results/detection.avi", cv2.cv.CV_FOURCC('M', 'P', 'E', 'G'), 29, (fwidth, fheight), 1)
-writer = cv2.VideoWriter("../Results/test_result.avi", cv2.cv.CV_FOURCC('M', 'P', 'E', 'G'), 29, (fwidth, fheight), 1)
+writer = cv2.VideoWriter("../Results/detection_street5.avi", cv2.cv.CV_FOURCC('M', 'P', 'E', 'G'), 29, (fwidth, fheight), 1)
+# writer = cv2.VideoWriter("../Results/test_result.avi", cv2.cv.CV_FOURCC('M', 'P', 'E', 'G'), 29, (fwidth, fheight), 1)
 aspect_ratio= fwidth*1.0/fheight
 
 element1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(13,13))
 element2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(11,11))
 match_candidates= []
+
+# for i in range(24):
+# 	cv_im = vid.read()[1]
 
 for i in range(nFrames):
 	cv_im = vid.read()[1]
@@ -26,10 +28,13 @@ for i in range(nFrames):
 	h, s = segmentRed(cv_im)
 	h = cv2.dilate(h, element1)
 	h = cv2.erode(h, element2)
+
+	# cv2.imwrite("../Temp/hue_frame_"+str(i)+".jpg", h)
 	draw_im = cv_im
 	
 	# Find circles on edges
 	edgeh = cv2.Canny(h, 128, 200)
+	# cv2.imwrite("../Temp/edge_frame_"+str(i)+".jpg", edgeh)
 	edgeCandidates = getCandidates(edgeh)
 
 	# Detect circles in the frame
--- trunk/Code/videobuilder.py
+++ trunk/Code/videobuilder.py
@@ -1,16 +1,16 @@
 import Image, cv2, os, numpy as np
 
-imgs = os.listdir("Test Sequence")
+imgs = os.listdir("../Video Sequence")
 
-im1 = cv2.imread("Test Sequence/"+imgs[0])
+im1 = cv2.imread("../Video Sequence/"+imgs[0])
 im1= np.asarray(im1)
 fheight = im1.shape[0]
 fwidth = im1.shape[1]
 
-writer = cv2.VideoWriter("street2.avi", cv2.cv.CV_FOURCC('M', 'P', 'E', 'G'), 20, (fwidth, fheight), 1)
+writer = cv2.VideoWriter("../Videos/street5.avi", cv2.cv.CV_FOURCC('M', 'P', 'E', 'G'), 29, (fwidth, fheight), 1)
 
 for img in imgs:
-	im = cv2.imread("Test Sequence/"+img)
+	im = cv2.imread("../Video Sequence/"+img)
 	im = np.asarray(im)
-	for i in range(10):
+	for i in range(2):
 		writer.write(im)
