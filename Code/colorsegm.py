38abf8dfb5acc10b1986ab2126c32051ee779d69
aya.skamel@gmail.com <aya.skamel@gmail.com@03c02e71-6919-b155-f245-4134f577a48b>
2013-07-14 14:41:11 +0000

    Generate more videos and applied detection to them

--- Code/colorsegm.py
+++ Code/colorsegm.py
@@ -1,13 +1,16 @@
-import cv2
+import cv2, numpy as np
 
 def segmentRed(cv_im):
 	hsv_im = cv2.cvtColor(cv_im, cv2.COLOR_BGR2HSV)
 	h, s, v = cv2.split(hsv_im)
 	r, sat_mask = cv2.threshold(s, 75, 1, cv2.THRESH_BINARY)
-	# cv2.imwrite("../Temp/sat_mask.jpg", sat_mask)
+	# cv2.imwrite("../Temp/sat_mask.jpg", np.multiply(sat_mask, 255))
 	r, h1 = cv2.threshold(h, 10, 255, cv2.THRESH_BINARY_INV)
+	# cv2.imwrite("../Temp/filtered_hue1.jpg", h1)
 	r, h2 = cv2.threshold(h, 230, 255, cv2.THRESH_BINARY)
+	# cv2.imwrite("../Temp/filtered_hue2.jpg", h2)
 	h = h1+h2
+	# cv2.imwrite("../Temp/filtered_hue.jpg", h)
 	h = h*sat_mask
 	# r, h = cv2.threshold(h, 128, 255, 0)
 	return h, sat_mask
